43d6e547c910a2994ee97d39a83d7f25b245bddd
htsmbslhdy <sam96basil@gmail.com>
2017-10-09 16:03:18 +0700

    ganti jadi hello basil

--- app/src/main/java/monpro/inf/praktikum/appgithub/MainActivity.java
+++ app/src/main/java/monpro/inf/praktikum/appgithub/MainActivity.java
@@ -2,12 +2,15 @@ package monpro.inf.praktikum.appgithub;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView teks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        teks = (TextView) findViewById(R.id.teks)
     }
 }
